901802b6008007d1aeb27421d9cdedc46494e4f6
balakinsergey83 <balakinsergey83@gmail.com>
2018-02-18 18:27:36 +0200

    DZ  4.2

--- Proba1/Program.cs
+++ Proba1/Program.cs
@@ -10,6 +10,42 @@ namespace Proba1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Сколько Вам лет");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a == 0 )
+            {
+                Console.WriteLine("В проэкте");
+            }
+
+            else if (a == 1 || a <= 5)
+            {
+                Console.WriteLine("Ребенок");
+            }
+
+            else if (a == 6 || a <= 15)
+            {
+                Console.WriteLine("Подросток");
+            }
+
+            else if (a == 16 || a <= 40)
+            {
+                Console.WriteLine("Взрослый");
+            }
+
+            else if (a == 41 || a <= 100)
+            {
+                Console.WriteLine("Пожилой");
+            }
+
+            else if (a >= 100)
+            {
+                Console.WriteLine("Долгожитель");
+            }
+
+            Console.ReadKey();
+
+
         }
     }
 }
